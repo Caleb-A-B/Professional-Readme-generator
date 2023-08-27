bc13276ce7ba75aecaad760c7fc7d76d307e34e6
caleb baca <calebabaca@gmail.com>
2023-08-27 10:10:22 -0700

    completed index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+//vairables for Qs:
+const inquirer = require("inquirer")
+const fs =require("fs")
+const util = require("util")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//vairables fpr connecting modsules and app
+const generateMarkdown = require("./utils/generateMarkdown")
+const licenseBadge = require("./utils/licenseBadge")
+const questions = require("./utils/questions")
+const writeFileAsync = util.promisify(fs.writeFile)
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers= await inquirer.createPromptModule(questions)
+        answers.licenseBadge = licenseBadge(answers.license)
+        let readMeData = generateMarkdown(answers)
+        await writeFileAsync("created-REAME.md", readMeData)
+    } catch (err) {
+        throw err
+    }
+}
 
 // Function call to initialize app
 init();
